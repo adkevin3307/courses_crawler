96b346ece598a208e500a3a898be361b3db91b41
adkevin3307 <adkevin3307@gmail.com>
2019-09-09 00:31:34 +0800

    update to multithread version

--- README.md
+++ README.md
@@ -4,12 +4,11 @@ Try to get all courses in NTOuO.
 # Enviroment
 * Python 3.7.3
 * selenium 3.141.0
-* tqdm 4.32.1
 * chromedriver 75.0.3770.8 (on Windows)
 
 # Quick Start
 * `python3 courses_crawler.py`
-* Input account and password.
+* Input amount of browsers.
 
 # Result
 Program will auto generate `courses.json`.
--- courses_crawler.py
+++ courses_crawler.py
@@ -1,38 +1,36 @@
 import json
 import time
-import tqdm
+import threading
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 
 from course import Course
 
-def get_courses(browser):
-    print('========== get courses ==========')
+def get_courses(browser, selections, thread_name):
     # defualt content -> mainFrame(outer)
     courses = []
     browser.switch_to.frame(browser.find_element_by_name('mainFrame'))
     browser.implicitly_wait(30)
-    # select fauclty and get all courses
-    options = list(map(lambda x: x.get_attribute('value'), Select(browser.find_element_by_id('Q_FACULTY_CODE')).options))
-    for i, option in enumerate(options):
+    for i, selection in enumerate(selections):
         # select and print current faculty
         select = Select(browser.find_element_by_id('Q_FACULTY_CODE'))
-        select.select_by_value(option)
-        print('{} / {}: {}'.format(i + 1, len(options), select.first_selected_option.text))
+        select.select_by_value(selection)
+        faculty_name = select.first_selected_option.text
         # click button and wait for render
         browser.find_element_by_id('QUERY_BTN1').click()
         time.sleep(2)
         # check total row amount
         total_rows = int(browser.find_element_by_id('PC_TotalRow').text)
+        print('{}: {} / {}, {}, {} rows'.format(thread_name, i + 1, len(selections), faculty_name, total_rows))
         if total_rows != 0:
             # set display rows to 1000
-            if i == 0:
+            if int(browser.find_element_by_id('PC_PageSize').get_attribute('value')) != 1000:
                 browser.execute_script('$("#PC_PageSize").attr("value", 1000)')
                 browser.find_element_by_id('PC_ShowRows').send_keys(Keys.ENTER)
                 time.sleep(1)
             # get course information
-            for row in tqdm.tqdm(range(2, 2 + total_rows)):
+            for row in range(2, 2 + total_rows):
                 # open fancybox
                 browser.execute_script('__doPostBack("DataGrid$ctl{:02d}$COSID", "")'.format(row))
                 time.sleep(0.5)
@@ -49,10 +47,53 @@ def get_courses(browser):
                 time.sleep(0.5)
     return courses
 
-if __name__ == '__main__':
-    with webdriver.Chrome() as browser:
+def parrallel(courses, selections):
+    thread_name = threading.current_thread().getName()
+    print('=============== {} Start ==============='.format(thread_name))
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option('excludeSwitches', ['enable-logging'])
+    with webdriver.Chrome(options = options) as browser:
         browser.get('https://ais.ntou.edu.tw/outside.aspx?mainPage=LwBBAHAAcABsAGkAYwBhAHQAaQBvAG4ALwBUAEsARQAvAFQASwBFADIAMgAvAFQASwBFADIAMgAxADEAXwAuAGEAcwBwAHgAPwBwAHIAbwBnAGMAZAA9AFQASwBFADIAMgAxADEA')
-        courses = get_courses(browser)
+        courses.extend(get_courses(browser, selections, thread_name))
+    print('=============== {} Done ==============='.format(thread_name))
 
+def get_selections(browser_amount):
+    selections = []
+    # set chrome options
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option('excludeSwitches', ['enable-logging'])
+    # get selections
+    with webdriver.Chrome(options = options) as browser:
+        browser.get('https://ais.ntou.edu.tw/outside.aspx?mainPage=LwBBAHAAcABsAGkAYwBhAHQAaQBvAG4ALwBUAEsARQAvAFQASwBFADIAMgAvAFQASwBFADIAMgAxADEAXwAuAGEAcwBwAHgAPwBwAHIAbwBnAGMAZAA9AFQASwBFADIAMgAxADEA')
+        print('=============== get selections ===============')
+        browser.switch_to.frame(browser.find_element_by_name('mainFrame'))
+        browser.implicitly_wait(30)
+        all_selections = list(map(lambda x: x.get_attribute('value'), Select(browser.find_element_by_id('Q_FACULTY_CODE')).options))
+    # split list to browser_amount part
+    average = len(all_selections) / browser_amount
+    index = 0.0
+    while index < len(all_selections):
+        selections.append(all_selections[int(index): int(index + average)])
+        index += average
+    # print selections
+    for selection in selections:
+        print(selection)
+    return selections
+
+if __name__ == '__main__':
+    # initialize variables
+    courses = []
+    threads = []
+    browser_amount = int(input('Amount of Browsers: '))
+    selections = get_selections(browser_amount)
+    # get courses
+    print('=============== get courses ===============')
+    for i in range(browser_amount):
+        threads.append(threading.Thread(target = parrallel, args = (courses, selections[i]), name = 'Thread {}'.format(i + 1)))
+        threads[i].start()
+    # wait all browsers and close them
+    for i in range(browser_amount):
+        threads[i].join()
+    # dump to courses.json
     with open('courses.json', 'w') as file:
         json.dump(courses, file, ensure_ascii = False, indent = 4, default = lambda x: x.__dict__)
